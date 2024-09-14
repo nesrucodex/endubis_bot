50709546fa7f4fac70b3aa8ed4d48109dd06a270
nesrucodex <155378885+nesrucodex@users.noreply.github.com>
2024-09-14 04:04:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -31,6 +31,14 @@ bot.onText(/\/start/, (msg) => {
   });
 });
 
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Bot server is running ..."
+  });
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
